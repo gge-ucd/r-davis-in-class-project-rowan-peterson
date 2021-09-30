2919eb50e4127051bbe095069a1d1e26849e2b9f
Rowan Peterson <rwpeterson@ucdavis.edu>
2021-09-30 14:43:56 -0700

    going through week 2 material

---
+++ wk_2_in_class_code.R
@@ -0,0 +1,21 @@
+weight_g <- c(50, 60, 65, 82)
+weight_g
+animals <- c("mouse", "rat", "dog")
+animals
+length(weight_g)
+length(animals)
+class(weight_g)
+str(weight_g)
+weight_g <- c(weight_g, 90) # add to the end of the vector
+weight_g <- c(30, weight_g) # add to the beginning of the vector
+weight_g
+typeof(weight_g)
+?typeof
+num_char <- c(1, 2, 3, "a") #mixed num/char is character
+class(num_char)
+num_logical <- c(1, 2, 3, TRUE) #mixed num/logical is numeric
+class(num_logical)
+char_logical <- c("a", "b", "c", TRUE)
+class(char_logical) #character
+tricky <- c(1, 2, 3, "4")
+class(tricky) #character
