bf672f8b1d8e21f9b32659f72f6ddc2265df0273
Rowan Peterson <rwpeterson@ucdavis.edu>
2021-10-28 14:36:32 -0700

    final draft

--- scripts/wk_5_homework.R
+++ scripts/wk_5_homework.R
@@ -14,6 +14,10 @@ head(surveys2)  ##success!
 surveys_wide <- pivot_wider(surveys2, names_from = "plot_type", values_from = "mean_hindfoot_length")
 surveys_wide   ##success!
 
+##sort by values in the control plot type
+surveys_wide2 <- surveys_wide %>% 
+  arrange(desc(Control))
+surveys_wide2
 ##Problem 2
 summary(surveys$weight)
 ##using ifelse
@@ -22,12 +26,13 @@ surveys3 <- surveys %>%
                               ifelse(weight >= 48, "large", "medium")))
   
 view(surveys3)  ##if weight is NA, the ifelse function automatically makes weight_cat NA
-
+##even if you don't specify
 ##using case_when
 surveys4 <- surveys %>% 
   mutate(weight_cat = case_when(weight <= 20 ~ "small",
                                 weight > 20 & weight < 48 ~ "medium",
                                 weight >= 48 ~ "large"))
+##if you did just TRUE ~ "large" for last category, NAs would also get "large"--be sure to specify when using case_when
 view(surveys4) ##case_when also automatically made weight_cat NA
 
 surveys5 <- surveys %>% 
